c163deed88dd669d90c70a2c3885d38e71840de6
Lasseeck <94852942+Lasseeck@users.noreply.github.com>
2021-11-22 22:29:47 +0100

    Quizmaster v1

---
+++ Quizmaster/gameMode.php
@@ -0,0 +1,16 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+require_once "$root" . "/functions.php";
+
+openSide();
+debugging("ja");
+
+if($_POST['schaetzen']==='on'){
+
+}elseif($_POST['buzzer']==="on"){
+    echo "Hallo";
+}elseif($_POST['allgemein']==='on'){
+
+}elseif($_POST['aufzaehlen']==='on'){
+
+}
--- Quizmaster/quizMasterInterface.php
+++ Quizmaster/quizMasterInterface.php
@@ -1,1 +1,14 @@
 <?php
+
+$root = $_SERVER['DOCUMENT_ROOT'];
+require_once "$root" . "/functions.php";
+
+openSide();
+debugging("ja");
+echo "<h1>Hi Quizmaster. Welches Spiel soll es sein? </h1>";
+echo "<form><button method='post' type='submit' name='schäetzen' value='on' formaction='gameMode.php'>Schätzfragen</button>";
+echo "<form><button method='post' type='submit' name='buzzer' value='on' formaction='gameMode.php'>Buzzerfragen</button>";
+echo "<form><button method='post' type='submit' name='allgemein' value='on' formaction='gameMode.php'>Allgemeinfragen</button>";
+echo "<form><button method='post' type='submit' name='aufzaehlen' value='on' formaction='gameMode.php'>Aufzählgame</button>";
+
+closeSide();
