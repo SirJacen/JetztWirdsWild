d693d8e9c59d55f8fce153900ee1f04c23bb932c
SirJacen <jan-ole.weiss@stud.hs-emden-leer.de>
2021-11-26 15:07:44 +0100

    CSS refined

--- Player/playerInterface.php
+++ Player/playerInterface.php
@@ -1,14 +1,10 @@
 <?php
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once "$root"."/functions.php";
+debugging("nein");
 
 openSide("..");
-debugging("nein");
+
 echo "<h1>Hi Player! Das Spiel startet gleich. Die Zuschauer wählen den ersten Fragenblock.</h1>";
-
-
-
-
-
 
 closeSide();
