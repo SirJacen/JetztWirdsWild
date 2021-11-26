1349de9d9e9a90c8bf4c3bca1a787074d2c4376c
Lasseeck <94852942+Lasseeck@users.noreply.github.com>
2021-11-26 15:47:50 +0100

    Quizmaster v4 - Grundgerüst

--- Quizmaster/gameMode.php
+++ Quizmaster/gameMode.php
@@ -1,21 +1,77 @@
 <?php
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once "$root" . "/functions.php";
+define("ResultPath", $root . "/Quimaster/Ergebnisse/results.json");
+define("QuestionPath", $root . "/Quimaster/currentBlock/");
 
 openSide();
 debugging("ja");
 
-echo "Hallo1";
-echo $_POST["schaetzen"];
-
-if($_POST['schaetzen']=='on'){
-    echo "Hallo";
-}elseif($_POST['buzzer']=="on"){
-    echo "Hallo";
-}elseif($_POST['allgemein']==='on'){
-    echo "Hallo";
-}elseif($_POST['aufzaehlen']==='on'){
-    echo "Hallo";
+if($_POST['result']=="on"){
+    if(checkResult()==false){
+        runGameOptions();
+    }
+    winnerCalc(ResultPath);
+    //Stats aufrufen ??
+    noGameOptions();
+}elseif($_POST['send']=="on"){
+    checkQuestions();
+    sendQuestions(splitQuestions());
+    runGameOptions();
+}elseif($_POST['check']=="on"){
+    checkQuestions();
+    sendGameOptions();
+}elseif{
+    noGameOptions();
 }
-echo "Hallo2";
+
+function checkResult() :bool{
+    $ret = false;
+    if(file_exists(ResultPath)){
+        $ret = true;
+    }
+    return $ret;
+}
+
+function splitQuestions(){
+    // JSON 1 Auslesen und die Fragen Aufteilen
+    // erste Frage in neue JSON 2 packen
+    // erste Frage aus JSON 1 löschen
+}
+
+function checkQuestions(): String {
+    $ret = "";
+    //nach Fragen im Ordner suchen
+    //Fragen ausgeben
+    return $ret;
+}
+
+function sendQuestions($path){
+
+}
+
+function noGameOptions(){
+    echo"
+        <form>
+              <button formmethod='post' type='submit' name='check' value='on' formaction='gameMode.php'>Fragen checken</button>
+              </form>
+    ";
+}
+
+function sendGameOptions(){
+    echo "
+        <form>
+              <button formmethod='post' type='submit' name='senden' value='on' formaction='gameMode.php'>Fragen senden</button>
+              <button formmethod='post' type='submit' name='check' value='on' formaction='gameMode.php'>Fragen checken</button>
+              </form>
+    ";
+}
+
+function runGameOptions(){
+    echo "Das Spiel läuft !<br>
+        <form>
+              <button formmethod='post' type='submit' name='result' value='on' formaction='gameMode.php'>Ergebnisse</button>
+              </form>
+    ";
+}
 closeSide();
