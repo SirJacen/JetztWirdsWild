df21910ddda83fb9285ab54a1c37958e00a4459f
Lasseeck <94852942+Lasseeck@users.noreply.github.com>
2021-11-29 14:31:41 +0100

    Quizmaster v5 - Checkquestions / Showquestions / Options(all) / Checkresults / Winnercalc

--- Quizmaster/gameMode.php
+++ Quizmaster/gameMode.php
@@ -1,8 +1,8 @@
 <?php
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once "$root" . "/functions.php";
-define("ResultPath", "$root" . "/Quimaster/Ergebnisse/results.json");
-define("QuestionPath", "$root" . "/Quimaster/currentBlock/");
+define("ResultPath", "$root" . "/Quizmaster/Ergebnisse/results.json");
+define("QuestionPath", "$root" . "\Quizmaster\currentBlock");
 
 openSide();
 debugging("ja");
@@ -21,7 +21,7 @@ if($_POST['result']=="on"){
 }elseif($_POST['check']=="on"){
     checkQuestions();
     sendGameOptions();
-}elseif{
+}else{
     noGameOptions();
 }
 
@@ -34,26 +34,80 @@ function checkResult() :bool{
 }
 
 function splitQuestions(){
-    $block = 1;
-    $file = QuestionPath . "questionsB". "$block".".json";
-    while ($block<5){
-        if(file_exists($file)){
-            break;
-        }
-        $block++;
-    }
-    echo json_decode(file_get_contents($file),true);
+
+    //echo json_decode(file_get_contents($file),true);
+    //$array=file_get_contents($file);
+    //var_dump(json_decode($array));
+    //makeChoices($array);
     // JSON 1 Auslesen und die Fragen Aufteilen
     // erste Frage in neue JSON 2 packen
     // erste Frage aus JSON 1 löschen
 }
 
-function checkQuestions(): String {
-    $ret = "";
+function makeChoices($array)
+{
+    foreach ($array as $question) {
+        $counter = 0;
+        echo "<input type='radio' id='question' name='$counter' value='on'>
+         <label for='html'>$question</label><br>";
+    }
+}
+
+function checkQuestions(){
+    $block = 1;
+    while ($block<5){
+        $file = QuestionPath . "\questionsB". "$block".".json";
+        if(file_exists($file)){
+            echo "ok";
+            break;
+        }
+        $block++;
+    }
+
+    showQuestions($block);
     //nach Fragen im Ordner suchen
     //Fragen ausgeben
-    return $ret;
 }
+
+function showQuestions($block)
+{
+    $questionArray = [];
+    array_push($questionArray, openJSON($block));
+    foreach ($questionArray as $key => $value) {
+        $newKey = $key + 1;
+        echo "<br>Block $newKey: ";
+        foreach ($value as $questionKey => $item) {
+            $questionIndex = $questionKey+1;
+            $questionName = $item['questionName'];
+            if (isset($item['answer'])) {
+                $questionAnswers = $item['answer'];
+                echo "<br>==> Frage $questionIndex: $questionName => $questionAnswers";
+            } elseif (isset($item['answerA'])){
+                $questionAnswerA = $item['answerA'];
+                $questionAnswerB = $item['answerB'];
+                $questionAnswerC = $item['answerC'];
+                $questionAnswerD = $item['answerD'];
+                echo "<br>==> Frage $questionIndex: $questionName => A: $questionAnswerA,
+                      B: $questionAnswerB, C: $questionAnswerC, D: $questionAnswerD";
+            }else {
+                echo "<br>==> Frage $questionIndex: $questionName";
+            }
+            checkImage($newKey, $questionName);
+        }
+    }
+}
+
+           /* if ($block == 1) {
+                //Schätzfragen
+            } elseif ($block == 2) {
+                echo $questionName;
+            } elseif ($block == 3) {
+
+            } elseif ($block == 4) {
+
+            }
+        }
+}*/
 
 function sendQuestions($path){
 
